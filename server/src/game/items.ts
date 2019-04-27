9047c64d3b42b54b943f70b87a156135ecacdf0c
Rahul Nair <rnair005@ucr.edu>
2019-04-27 16:22:44 -0700

    updated items.ts

--- server/src/game/items.ts
+++ server/src/game/items.ts
@@ -19,4 +19,50 @@ itemList.push(
         points: 1,
         tags: ['computer keyboard', 'laptop', 'monitor', 'computer screen']
     }
+    {
+        name: 'Bottle',
+        points: 1,
+        tags: ['bottle', 'water bottle', 'plastic bottle']
+    }
+    {
+        name: 'Phone',
+        points: 1,
+        tags: ['mobile phone', 'phone', 'iphone', 'smartphone']
+
+    }
+    {
+        name: 'Keyboard',
+        points: 1,
+        tags: ['keyboard', 'computer keyboard']
+
+    }
+
+    {
+      name: 'Mouse',
+      points: 1,
+      tags: ['mouse', 'computer mouse']
+    }
+
+
+    {
+      name: 'Glasses',
+      points: 1,
+      tags: ['glasses', 'sunglasses', 'eyewear']
+    }
+
+    {
+      name: 'Backpack',
+      points: 1,
+      tags: ['bag', 'backpack', 'baggage']
+    }
+
+    {
+      name: 'Soda Can',
+      points: 1,
+      tags: ['tin can', 'aluminum can', 'beverage can', 'soda can']
+    }
+
+
+
+
 )
