99b8990c6d2ddc8ddb6d7327c62cbf2d4015a0a5
Luke Chatton <chattonluke@gmail.com>
2019-04-27 01:41:36 -0700

    camera works

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,35 +1,74 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { RNCamera, FaceDetector } from 'react-native-camera';
 
 export default class extends React.Component {
-    constructor() {
-
+    constructor(props) {
+        super(props);
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Game Screen</Text>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                        title: 'Permission to use audio recording',
+                        message: 'We need your permission to use your audio',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                        console.log(barcodes);
+                    }}
+                />
+                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+                        <Text style={{ fontSize: 14 }}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
+
+    takePicture = async function () {
+        if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options);
+            console.log(data.uri);
+        }
+    };
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        flexDirection: 'column',
+        backgroundColor: 'black',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
     },
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { NavigationService } from '../providers/NavigationService';
 
 export default class extends React.Component {
     constructor(props) {
@@ -10,6 +12,11 @@ export default class extends React.Component {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Welcome to FindIt!</Text>
+                <TouchableOpacity onPress={() => NavigationService.navigate('Game') }>
+                    <View style={styles.testButton}>
+                        <Text style={styles.testButtonText}>Test Camera</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -32,4 +39,13 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    testButton: {
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: '#26292d'
+    },
+    testButtonText: {
+        color: '#fff',
+        fontWeight: '600'
+    }
 });
