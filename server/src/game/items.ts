6c18e442aebcfeb437507c35676d546020eb6bb4
Luke Chatton <chattonluke@gmail.com>
2019-04-27 16:27:00 -0700

    fix syntax

--- server/src/game/items.ts
+++ server/src/game/items.ts
@@ -18,51 +18,42 @@ itemList.push(
         name: 'Computer',
         points: 1,
         tags: ['computer keyboard', 'laptop', 'monitor', 'computer screen']
-    }
+    },
     {
         name: 'Bottle',
         points: 1,
         tags: ['bottle', 'water bottle', 'plastic bottle']
-    }
+    },
     {
         name: 'Phone',
         points: 1,
         tags: ['mobile phone', 'phone', 'iphone', 'smartphone']
 
-    }
+    },
     {
         name: 'Keyboard',
         points: 1,
         tags: ['keyboard', 'computer keyboard']
 
-    }
-
+    },
     {
       name: 'Mouse',
       points: 1,
       tags: ['mouse', 'computer mouse']
-    }
-
-
+    },
     {
       name: 'Glasses',
       points: 1,
       tags: ['glasses', 'sunglasses', 'eyewear']
-    }
-
+    },
     {
       name: 'Backpack',
       points: 1,
       tags: ['bag', 'backpack', 'baggage']
-    }
-
+    },
     {
       name: 'Soda Can',
       points: 1,
       tags: ['tin can', 'aluminum can', 'beverage can', 'soda can']
     }
-
-
-
-
 )
